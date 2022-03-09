b7bf16bc0b20a112e12ab3ae7b21f6058785d875
Gan <rclamoc@iskolarngbayan.pup.edu.ph>
2022-03-09 12:14:31 +0800

    initial commit

---
+++ animate.js
@@ -0,0 +1,182 @@
+const draggableArrow = document.getElementById("dragArrow");
+const intro = document.getElementById("intro");
+const introMain = document.getElementById("introMain");
+const contents = document.getElementById("contents");
+
+const body = document.getElementsByTagName("body")[0];
+const moon = document.getElementById("moon");
+const stars = document.getElementById("stars");
+const m1 = document.getElementById("m1");
+const m2 = document.getElementById("m2");
+const m3 = document.getElementById("m3");
+const ground = document.getElementById("ground");
+const section = document.getElementById("section");
+let isSmall = false;
+
+if (window.innerWidth <= 768) {
+  isSmall = true;
+}
+window.scrollTo(0, 0);
+// body.style = "height: 100vh; overflow: hidden";
+contents.style.display = "none";
+
+// draggableArrow.onclick = () => {
+//   main.removeAttribute("style");
+// };
+
+const checkDrag = (e) => {
+  dragAmount = draggableArrow.getBoundingClientRect().y;
+  if (dragAmount > 50) {
+    intro.style.top = draggableArrow.getBoundingClientRect().y - 50 + "px";
+  }
+  if (dragAmount > 75) {
+    intro.style.opacity = 1 - dragAmount / screen.height;
+  } else {
+    intro.style.opacity = 1;
+  }
+  if (dragAmount > screen.height / 2) {
+    intro.classList.add("closeIntro");
+    draggableArrow.remove();
+    setTimeout(() => {
+      introMain.remove();
+      body.removeAttribute("style");
+      section.style.overflowY = "hidden";
+      contents.removeAttribute("style");
+    }, 400);
+  }
+};
+
+const stopDrag = (e) => {
+  draggableArrow.style.top = "25px";
+  intro.style.top = "0";
+  intro.style.opacity = 1;
+};
+
+if (!isSmall) $("#dragArrow").draggable({ axis: "y", drag: checkDrag, stop: stopDrag });
+else {
+  draggableArrow.remove();
+  setTimeout(() => {
+    opacityValue = 0;
+    const hide = setInterval(() => {
+      if (opacityValue > 1) {
+        introMain.remove();
+        body.removeAttribute("style");
+        section.style.overflowY = "hidden";
+        contents.removeAttribute("style");
+        clearInterval(hide);
+      }
+      introMain.style.opacity = 1 - opacityValue;
+      opacityValue += 0.1;
+    }, 50);
+  }, 1800);
+}
+
+const titleSkills = document.getElementById("titleSkills");
+const navBtn = document.querySelectorAll("nav a");
+const navAnimation = document.getElementById("navAnimation");
+let currentTab = navBtn[0];
+const navbar = document.getElementById("navbar");
+
+// console.log(navbar.getBoundingClientRect.height);
+
+navAnimation.style.left = currentTab.offsetLeft + "px";
+navAnimation.style.width = currentTab.offsetWidth + "px";
+currentTab.onmouseover = () => {
+  currentTab.style.color = "#010101";
+};
+
+navBtn.forEach((element) => {
+  element.onclick = () => {
+    if (element === currentTab) return;
+    // currentTab.classList.remove("focus");
+    // currentTab.removeAttribute("style");
+    // navAnimation.style.left = element.offsetLeft + "px";
+    // navAnimation.style.width = element.offsetWidth + "px";
+    // currentTab = element;
+    // currentTab.onmouseover = () => {
+    //   currentTab.style.color = "#010101";
+    // };
+    // currentTab.classList.add("focus");
+    if (element === navBtn[0]) {
+      window.scrollTo(0, 0);
+    } else if (element === navBtn[1]) {
+      window.scrollBy(0, titleSkills.getBoundingClientRect().top - navbar.getBoundingClientRect().height - 20);
+    } else if (element === navBtn[2]) {
+      window.scrollBy(0, titleProjects.getBoundingClientRect().top - navbar.getBoundingClientRect().height - 20);
+    }
+    // else if (element === navBtn[2]) {
+    //   window.scrollBy(0, titleProjects.getBoundingClientRect().top - 100);
+    // }
+  };
+});
+
+window.onscroll = () => {
+  if (titleProjects.getBoundingClientRect().top <= 150) {
+    currentTab.classList.remove("focus");
+    currentTab.removeAttribute("style");
+    navAnimation.style.left = navBtn[2].offsetLeft + "px";
+    navAnimation.style.width = navBtn[2].offsetWidth + "px";
+    currentTab = navBtn[2];
+    currentTab.onmouseover = () => {
+      currentTab.style.color = "#010101";
+    };
+    currentTab.classList.add("focus");
+  } else if (titleSkills.getBoundingClientRect().top <= 150) {
+    currentTab.classList.remove("focus");
+    currentTab.removeAttribute("style");
+    navAnimation.style.left = navBtn[1].offsetLeft + "px";
+    navAnimation.style.width = navBtn[1].offsetWidth + "px";
+    currentTab = navBtn[1];
+    currentTab.onmouseover = () => {
+      currentTab.style.color = "#010101";
+    };
+    currentTab.classList.add("focus");
+  } else if (titleSkills.getBoundingClientRect().top > 150) {
+    currentTab.classList.remove("focus");
+    currentTab.removeAttribute("style");
+    navAnimation.style.left = navBtn[0].offsetLeft + "px";
+    navAnimation.style.width = navBtn[0].offsetWidth + "px";
+    currentTab = navBtn[0];
+    currentTab.onmouseover = () => {
+      currentTab.style.color = "#010101";
+    };
+    currentTab.classList.add("focus");
+  }
+
+  let scrollValue = window.scrollY;
+  stars.style.top = scrollValue * -0.4 + "px";
+  stars.style.left = scrollValue * 0.3 + "px";
+  moon.style.top = scrollValue * 0.6 + "px";
+  m3.style.top = scrollValue * 0.25 + "px";
+  if (window.innerWidth > 1400) {
+    ground.style.top = scrollValue * -0.1 + "px";
+  }
+  m1.style.top = scrollValue * 0.3 + "px";
+  m2.style.top = scrollValue * 0.05 + "px";
+};
+
+const btnSkills = document.getElementById("btnSkills");
+
+// btnSkills.onmouseover = () => {
+//   console.log("ngo");
+//   window.scrollBy(0, titleSkills.getBoundingClientRect().top - 100);
+//   // console.log(titleSkills.getBoundingClientRect().top);
+// };
+
+// const scroll = () => {
+//   setTimeout({
+//     window.scrollTo
+//   },100)
+// }
+
+const contact = document.querySelectorAll(".contact");
+
+contact.forEach((element) => {
+  element.onclick = () => {
+    navigator.clipboard.writeText(element.textContent);
+    element.classList.add("copied");
+    setTimeout(() => {
+      element.classList.remove("copied");
+    }, 750);
+  };
+});
---
+++ controller.js
@@ -0,0 +1,239 @@
+let controller = new ScrollMagic.Controller();
+let timeline = new TimelineMax();
+let timeline2 = new TimelineMax();
+let timeline3 = new TimelineMax();
+
+timeline
+  .to("#introName", 1, { x: 350, opacity: 0 })
+  .to("#introSub", 1, { x: -100, opacity: 0, zIndex: 0 }, "-=1")
+  .to("#navbar", 1, { background: "#424242" }, "-=1")
+  .to("#navName", 1, { opacity: 1 }, "-=1");
+
+let scene = new ScrollMagic.Scene({
+  // triggerElement: "#contents",
+  duration: "100%",
+  triggerHook: 0.8,
+})
+  .setTween(timeline)
+  .addTo(controller);
+
+timeline2
+  .fromTo("#titleAbout", 10, { opacity: 0 }, { opacity: 1 })
+  .fromTo("#pinIcon", 10, { y: -50, opacity: 0 }, { y: 0, opacity: 1 })
+  .fromTo("#txtLocation", 1, { x: -50, opacity: 0 }, { x: 0, opacity: 1 })
+  .fromTo("#educIcon", 10, { y: -50, opacity: 0 }, { y: 0, opacity: 1 })
+  .fromTo("#txtEduc", 1, { x: -50, opacity: 0 }, { x: 0, opacity: 1 })
+  .fromTo("#workIcon", 10, { y: -50, opacity: 0 }, { y: 0, opacity: 1 })
+  .fromTo("#txtWork", 1, { y: -50, opacity: 0 }, { y: 0, opacity: 1 })
+  .fromTo("#txtWork1", 10, { y: -50, opacity: 0 }, { y: 0, opacity: 1 })
+  .fromTo("#txtAbout", 10, { y: -50, opacity: 0 }, { y: 0, opacity: 1 });
+
+let scene2 = new ScrollMagic.Scene({
+  triggerElement: "#contentAbout",
+  duration: "40%",
+  triggerHook: 0.5,
+})
+  .setTween(timeline2)
+  // .addIndicators()
+  .addTo(controller);
+
+timeline3
+  .fromTo("#titleSkills", 10, { opacity: 0 }, { opacity: 1 })
+  .fromTo("#txtSkills", 10, { y: -30, opacity: 0 }, { y: 0, opacity: 1 });
+let scene3 = new ScrollMagic.Scene({
+  triggerElement: "#txtAbout",
+  duration: "40%",
+  triggerHook: 0.3,
+})
+  .setTween(timeline3)
+  .addTo(controller);
+
+const imgPy = document.getElementById("imgPy");
+const imgJs = document.getElementById("imgJs");
+const imgJa = document.getElementById("imgJa");
+const imgPh = document.getElementById("imgPh");
+const imgC = document.getElementById("imgC");
+const imgMy = document.getElementById("imgMy");
+const imgMo = document.getElementById("imgMo");
+const imgRe = document.getElementById("imgRe");
+const imgGear = document.getElementById("imgGear");
+
+const boxz = document.getElementById("boxz");
+const imgSkillContainer = document.getElementById("imgSkillContainer");
+
+let isMouseOut = false;
+let imgContainerW = imgSkillContainer.getBoundingClientRect().width / 4;
+let pyTop = imgPy.offsetTop;
+let pyLeft = imgPy.offsetLeft;
+
+window.onresize = () => {
+  resizeImg();
+};
+
+const resizeImg = () => {
+  let shrinkValue = 1;
+  if (window.innerWidth > 992) {
+    imgGear.setAttribute("height", 200);
+    imgGear.setAttribute("width", 200);
+    shrinkValue = 1;
+    resizeOperation(100);
+  } else if (window.innerWidth <= 992) {
+    imgGear.setAttribute("height", 125);
+    imgGear.setAttribute("width", 125);
+    shrinkValue = 0.75;
+    resizeOperation(75);
+  }
+
+  // imgPy.setAttribute("width", imgPy.width * shrinkValue);
+  // imgPy.setAttribute("height", imgPy.height * shrinkValue);
+  // imgJs.setAttribute("width", imgPy.width * shrinkValue);
+  // imgJs.setAttribute("height", imgPy.height * shrinkValue);
+  // imgJa.setAttribute("width", imgPy.width * shrinkValue);
+  // imgJa.setAttribute("height", imgPy.height * shrinkValue);
+  // imgPh.setAttribute("height", imgPy.height * shrinkValue);
+  // imgC.setAttribute("width", imgPy.width * shrinkValue);
+  // imgC.setAttribute("height", imgPy.height * shrinkValue);
+  // imgMy.setAttribute("height", imgPy.height * shrinkValue);
+  // imgMo.setAttribute("height", imgPy.height * shrinkValue);
+  // imgRe.setAttribute("width", imgPy.width * shrinkValue);
+  // imgRe.setAttribute("height", imgPy.height * shrinkValue);
+};
+
+const resizeOperation = (size) => {
+  imgPy.setAttribute("width", size);
+  imgPy.setAttribute("height", size);
+  imgJs.setAttribute("width", size);
+  imgJs.setAttribute("height", size);
+  imgJa.setAttribute("width", size);
+  imgJa.setAttribute("height", size);
+  imgPh.setAttribute("height", size);
+  imgC.setAttribute("width", size);
+  imgC.setAttribute("height", size);
+  imgMy.setAttribute("height", size);
+  imgMo.setAttribute("height", size);
+  imgRe.setAttribute("width", size);
+  imgRe.setAttribute("height", size);
+  imgContainerW = imgSkillContainer.getBoundingClientRect().width / 4;
+  pyTop = imgPy.offsetTop;
+  pyLeft = imgPy.offsetLeft;
+};
+
+resizeImg();
+
+imgSkillContainer.onmouseover = () => {
+  // console.log(imgSkillContainer.getBoundingClientRect().width / 4);
+  if (window.innerWidth < 1200) {
+    // console.log(imgPy.offsetLeft);
+    // imgPy.setAttribute("width", imgPy.width / 2);
+    // imgPy.setAttribute("height", imgPy.height / 2);
+    // imgPy.setAttribute("width", toString(imgPy.width / 2));
+  }
+  if (!isMouseOut) {
+    imgPy.style.top = pyTop - 100 + "px";
+    imgJs.style.top = pyTop - 100 + "px";
+    imgJa.style.top = pyTop - 100 + "px";
+    imgPh.style.top = pyTop - 100 + "px";
+    imgC.style.top = pyTop + 100 + "px";
+    imgMy.style.top = pyTop + 100 + "px";
+    imgMo.style.top = pyTop + 100 + "px";
+    imgRe.style.top = pyTop + 100 + "px";
+    imgPy.style.left = pyLeft - imgContainerW * 1.5 + "px";
+    imgJs.style.left = pyLeft - imgContainerW * 0.5 + "px";
+    imgJa.style.left = pyLeft + imgContainerW * 0.5 + "px";
+    imgPh.style.left = pyLeft + imgContainerW * 1.5 + "px";
+    imgC.style.left = pyLeft - imgContainerW * 1.5 + "px";
+    imgMy.style.left = pyLeft - imgContainerW * 0.5 + "px";
+    imgMo.style.left = pyLeft + imgContainerW * 0.5 + "px";
+    imgRe.style.left = pyLeft + imgContainerW * 1.5 + "px";
+    // console.log(imgPy.offsetTop);
+    // console.log(imgPy.offsetLeft);
+    isMouseOut = true;
+    imgPy.classList.add("skillLogoShow");
+    imgJs.classList.add("skillLogoShow");
+    imgJa.classList.add("skillLogoShow");
+    imgPh.classList.add("skillLogoShow");
+    imgC.classList.add("skillLogoShow");
+    imgMy.classList.add("skillLogoShow");
+    imgMo.classList.add("skillLogoShow");
+    imgRe.classList.add("skillLogoShow");
+    imgGear.classList.add("gearRotate");
+  }
+};
+imgSkillContainer.onmouseleave = () => {
+  imgPy.removeAttribute("style");
+  imgJs.removeAttribute("style");
+  imgJa.removeAttribute("style");
+  imgPh.removeAttribute("style");
+  imgC.removeAttribute("style");
+  imgMy.removeAttribute("style");
+  imgMo.removeAttribute("style");
+  imgRe.removeAttribute("style");
+
+  imgPy.classList.remove("skillLogoShow");
+  imgJs.classList.remove("skillLogoShow");
+  imgJa.classList.remove("skillLogoShow");
+  imgPh.classList.remove("skillLogoShow");
+  imgC.classList.remove("skillLogoShow");
+  imgMy.classList.remove("skillLogoShow");
+  imgMo.classList.remove("skillLogoShow");
+  imgRe.classList.remove("skillLogoShow");
+  imgGear.classList.remove("gearRotate");
+
+  isMouseOut = false;
+};
+
+// const projImg = document.getElementById("projImg");
+const projImgs = document.querySelectorAll("#projImg");
+
+projImgs.forEach((projImg) => {
+  projImg.onmouseover = () => {
+    projImg.classList.add("cardImgZoom");
+  };
+  projImg.onmouseleave = () => {
+    projImg.classList.remove("cardImgZoom");
+  };
+});
+
+// const cardBtn = document.getElementById("cardBtn");
+const cardBtns = document.querySelectorAll("#cardBtn");
+// const cardBtnBack = document.getElementById("cardBtnBack");
+const cardBtnsBack = document.querySelectorAll("#cardBtnBack");
+
+cardBtns.forEach((cardBtn) => {
+  cardBtn.onclick = () => {
+    cardBtn.parentNode.parentNode.classList.add("cardRotate");
+    setTimeout(() => {
+      cardBtn.parentNode.parentNode.firstElementChild.classList.add("d-none");
+      cardBtn.parentNode.parentNode.lastElementChild.classList.remove("d-none");
+    }, 150);
+  };
+});
+
+cardBtnsBack.forEach((cardBtnBack) => {
+  cardBtnBack.onclick = () => {
+    cardBtnBack.parentNode.parentNode.classList.remove("cardRotate");
+    setTimeout(() => {
+      cardBtnBack.parentNode.parentNode.firstElementChild.classList.remove("d-none");
+      cardBtnBack.parentNode.parentNode.lastElementChild.classList.add("d-none");
+    }, 150);
+  };
+});
+
+// cardBtn.onclick = () => {
+//   cardBtn.parentNode.parentNode.classList.add("cardRotate");
+//   setTimeout(() => {
+//     cardBtn.parentNode.parentNode.firstElementChild.classList.add("d-none");
+//     cardBtn.parentNode.parentNode.lastElementChild.classList.remove("d-none");
+//   }, 150);
+// };
+
+// cardBtnBack.onclick = () => {
+//   cardBtnBack.parentNode.parentNode.classList.remove("cardRotate");
+//   setTimeout(() => {
+//     cardBtnBack.parentNode.parentNode.firstElementChild.classList.remove("d-none");
+//     cardBtnBack.parentNode.parentNode.lastElementChild.classList.add("d-none");
+//   }, 150);
+// };
+
+//################ reference for offset scroll
+// https://stackoverflow.com/questions/49820013/javascript-scrollintoview-smooth-scroll-and-offset
